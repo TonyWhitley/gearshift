9735009ef8414632d1c9e2565e97c77ef9e1b872
TonyWhitley <Tony_Whitley@hotmail.com>
2019-08-15 19:15:01 +0100

    Create git_sub.cmd
    git bash commands to load submodules

---
+++ git_sub.cmd
@@ -0,0 +1,3 @@
+echo git bash commands to load submodules
+git submodule add https://github.com/TonyWhitley/pyDirectInputKeySend                                                 
+git submodule add https://github.com/TonyWhitley/pyRfactor2SharedMemory                                                  

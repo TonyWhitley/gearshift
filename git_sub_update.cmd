47f95195aa8a7b0306b72470b1f18ed5f34080ee
TonyWhitley <Tony_Whitley@hotmail.com>
2019-08-23 11:25:49 +0100

    (update submodules pyRfactor2SharedMemory and pyDirectInputKeySend)

--- git_sub_update.cmd
+++ git_sub_update.cmd
@@ -2,6 +2,6 @@ echo git bash commands to update submodules to latest
 rem Automated login
 git config --global credential.helper wincred
 
-git submodule update --remote --merge pyDirectInputKeySend                                                 
-git submodule update --remote --merge pyRfactor2SharedMemory                               
+git submodule update --remote --merge --recursive pyDirectInputKeySend                                                 
+git submodule update --remote --merge --recursive pyRfactor2SharedMemory                               
 git push
